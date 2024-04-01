5c3b7a049d734eb898811e863d9ddb3b4033c4e2
can caliskan <podcastwerv2017@gmail.com>
2024-04-01 09:36:23 +0300

    img folder, header, hero

--- img/tailwind-landing-page-main/tailwind.config.js
+++ img/tailwind-landing-page-main/tailwind.config.js
@@ -1,16 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screens:{
-      sm:'480px',
+    screens: {
+      sm: '480px',
       md: '768px',
       lg: '976px',
       xl: '1440px',
-
     },
     extend: {
-      colors:{
+      colors: {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
@@ -19,9 +17,8 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
-      }
+      },
     },
   },
   plugins: [],
 }
-
